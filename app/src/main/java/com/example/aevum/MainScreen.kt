75638f2c796cabdd7b151a09289818d1c2d8daf8
Patrick Lee <patricklee@Patricks-MacBook-Air.local>
2025-04-27 21:30:15 +0300

    App's Template with Bottom Bar and switching pages functionality.

---
+++ app/src/main/java/com/example/aevum/MainScreen.kt
@@ -0,0 +1,69 @@
+package com.example.aevum
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.example.aevum.pages.TemplatePage
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+
+    val NavItemList = listOf<NavItem>(
+        NavItem("Page 1", Icons.Default.Home),
+        NavItem("Page 2", Icons.Default.Person),
+        NavItem("Page 3", Icons.Default.Settings)
+    )
+
+    var selectedIndex by remember {
+        mutableIntStateOf(0)
+    }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar {
+                NavItemList.forEachIndexed { index, navItem ->
+                    NavigationBarItem(
+                        selected = selectedIndex == index,
+                        onClick = {
+                            selectedIndex = index
+                        },
+                        icon = {
+                            Icon(imageVector = navItem.icon, contentDescription = "Icon")
+                        },
+                        label = {
+                            Text(text = navItem.label)
+                        }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        ContentScreen(modifier = Modifier.padding(innerPadding), selectedIndex)
+    }
+}
+
+@Composable
+fun ContentScreen(modifier: Modifier = Modifier, selectedIndex: Int) {
+    when(selectedIndex) {
+        0 -> TemplatePage()
+        1 -> TemplatePage()
+        2 -> TemplatePage()
+    }
+}
